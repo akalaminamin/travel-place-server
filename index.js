9ed50f76a4f487b1c85d3a0d6a3e7016cf3ce5b1
alamin <akalaminamin91@gmail.com>
2021-10-31 20:03:12 +0600

    change some function and modify index js file

--- index.js
+++ index.js
@@ -1,102 +1,80 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient } = require('mongodb');
-const ObjectId  = require("mongodb").ObjectId;
-require('dotenv').config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.deaij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
-    try {
-        client.connect();
-        const database = client.db("Travel");
-        const travelServiceCollection = database.collection("travel Service");
-        const placeOrderCollection = database.collection("Place order");
-        const bookNowCollection = database.collection("BookNow");
+  try {
+    client.connect();
+    const database = client.db("Travel");
+    const travelServiceCollection = database.collection("travel Service");
+    const placeOrderCollection = database.collection("Place order");
 
-        // post api
-        app.post("/services", async(req, res)=>{
-            const cursor = req.body;
-            const result = await travelServiceCollection.insertOne(cursor);
-            res.json(result);
-        })
-        app.get("/services", async(req, res)=>{
-            const result = await travelServiceCollection.find({}).toArray();
-            res.json(result)
-        })
+    // post api
+    app.get("/services", async (req, res) => {
+      const result = await travelServiceCollection.find({}).toArray();
+      res.json(result);
+    });
 
-        //  place order post in data base
-        app.post("/placeOrder", async(req, res)=>{
-            const cursor = req.body;
-            const result = await placeOrderCollection.insertOne(cursor);
-            res.json(result)
-        })
+    // post api
+    app.get("/myOrder", async (req, res) => {
+      const result = await placeOrderCollection.find({}).toArray();
+      res.json(result);
+    });
 
-        // post data  in Book now collection
-        app.post("/bookNow", async(req, res) =>{
-            const cursor = req.body;
-            const result = await bookNowCollection.insertOne(cursor)
-            console.log(result)
-            res.json(result)
-        })
+    // Delete manage order api
+    app.delete("/myOrder/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await placeOrderCollection.deleteOne(filter);
+      console.log(result);
+      res.json(result);
+    });
 
-        // get api in book now in dashboard
-        app.get("/bookNow", async(req, res)=>{
-            const result = await bookNowCollection.find({}).toArray();
-            // console.log(result)
-            res.json(result)
-        })
+    // add service post api
+    app.post("/addservice", async (req, res) => {
+      const cursor = req.body;
+      const result = await travelServiceCollection.insertOne(cursor);
+      res.json(result);
+    });
 
-        // delete dashbard item
-        app.delete("/dashboard/:id", async(req, res) =>{
-            const id = req.params.id;
-            const filter = {_id:id};
-            const result = await bookNowCollection.deleteOne(filter);
-            // console.log(result)
-            res.send(result)
-        })
-        // Get my order 
-        app.get("/bookNow/:email", async(req, res)=>{
-            const email = req.params.email;
-            const result = await bookNowCollection.find({email}).toArray();
-            // console.log(email)
-            res.json(result)
-        })
+    // placeorder post api
+    app.post("/placeOrder", async (req, res) => {
+      const cursor = req.body;
+      const result = await placeOrderCollection.insertOne(cursor);
+      res.json(result);
+    });
 
-        // add new tour list 
-        app.post("/addservice", async(req, res) =>{
-            const cursor = req.body;
-            const result = await travelServiceCollection.insertOne(cursor);
-            res.json(result);
-        })
-
-        // Delete my order item
-        app.delete("/bookNow/:id", async(req, res) =>{
-            const id = req.params.id;
-            const filter = {_id:id};
-            const result = await bookNowCollection.deleteOne(filter);
-            // console.log(result);
-            res.json(result);
-        })
-
-        
-    } catch {
-        // await client.close()
-    }
+    // Delete post api
+    app.delete("/placeOrder/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await travelServiceCollection.deleteOne(filter);
+      res.json(result);
+    });
+  } catch {
+    // await client.close()
+  }
 }
 
 run().catch(console.dir);
+
 app.get("/", (req, res) => {
-    res.send("Server is running");
-})
+  res.send("Server is running");
+});
 
 app.listen(port, () => {
-    console.log("Server running port is", port);
-})
-
+  console.log("Server running port is", port);
+});
