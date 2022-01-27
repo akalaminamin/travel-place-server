ab71fd41513041f9c31899e107672774e1d1f302
Al Amin <akalaminamin91@gmail.com>
2022-01-27 12:09:43 -0800

    added some functionality

--- index.js
+++ index.js
@@ -14,62 +14,49 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 async function run() {
   try {
     client.connect();
-    const database = client.db("Travel");
-    const travelServiceCollection = database.collection("travel Service");
-    const placeOrderCollection = database.collection("Place order");
+    const database = client.db("trveller");
+    const travelServiceCollection = database.collection("travel-Service");
+    const myPostsCollection = database.collection("myPosts");
+    const adminCollection = database.collection("admin");
 
     // post api
     app.get("/services", async (req, res) => {
       const result = await travelServiceCollection.find({}).toArray();
       res.json(result);
     });
-
-    // post api
-    app.get("/myOrder", async (req, res) => {
-      const result = await placeOrderCollection.find({}).toArray();
-      res.json(result);
-    });
-
-    // Delete manage order api
-    app.delete("/myOrder/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: ObjectId(id) };
-      const result = await placeOrderCollection.deleteOne(filter);
-      console.log(result);
-      res.json(result);
-    });
-
-    // add service post api
-    app.post("/addservice", async (req, res) => {
+    
+    // add place post api
+    app.post("/services/addPlace", async (req, res) => {
       const cursor = req.body;
       const result = await travelServiceCollection.insertOne(cursor);
       res.json(result);
     });
 
-    // placeorder post api
-    app.post("/placeOrder", async (req, res) => {
-      const cursor = req.body;
-      const result = await placeOrderCollection.insertOne(cursor);
+    // Delete manage order api
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await travelServiceCollection.deleteOne(filter);
       console.log(result);
-      res.json(result);
+      res.json(result); 
     });
 
     // update post status
-    app.put("/myOrder/:id", async (req, res) => {
+    app.put("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const status = req.body;
+      console.log(status);
       const options = { upsert: true };
       const updateStatus = {
         $set: {
           status: "Approved",
         },
       };
-      const result = await placeOrderCollection.updateOne(
+      const result = await travelServiceCollection.updateOne(
         query,
         updateStatus,
         options
@@ -77,13 +64,41 @@ async function run() {
       res.json(result);
     });
 
-    // Delete post api
-    app.delete("/placeOrder/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: ObjectId(id) };
-      const result = await travelServiceCollection.deleteOne(filter);
+    // post api
+    app.get("/myOrder", async (req, res) => {
+      const result = await myPostsCollection.find({}).toArray();
       res.json(result);
     });
+
+    // admin post api
+    app.post("/admin", async(req, res)=>{
+      const cursor = req.body;
+      const result = await adminCollection.insertOne(cursor);
+      res.json(result);
+    })
+    // admin get api
+    app.get("/admin", async(req, res)=>{
+      const result = await adminCollection.find({}).toArray();
+      res.json(result);
+    })
+
+
+    // placeorder post api
+    // app.post("/placeOrder", async (req, res) => {
+    //   const cursor = req.body;
+    //   const result = await myPostsCollection.insertOne(cursor);
+    //   console.log(result);
+    //   res.json(result);
+    // });
+
+  
+    // Delete post api
+    // app.delete("/placeOrder/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: ObjectId(id) };
+    //   const result = await travelServiceCollection.deleteOne(filter);
+    //   res.json(result);
+    // });
   } catch {
     // await client.close()
   }
